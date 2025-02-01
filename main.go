611f3361516ed47df6b63d17c8537edc865ba99f
SinclairPythonAkoto <sinclair.python@gmail.com>
2025-02-01 12:25:29 +0000

    Function created to load environment variables

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"os"
+
+	"github.com/Bevs-n-Devs/dearmatrongo/env"
 	"github.com/Bevs-n-Devs/dearmatrongo/handlers"
 	"github.com/Bevs-n-Devs/dearmatrongo/logs"
 )
 
 func main() {
 	go logs.ProcessLogs()
-	logs.Log(logs.INFO, "hello world, hello Yaw!")
+	// load environment variables from the .env file
+	err := env.LoadEnv("env/.env")
+	if err != nil {
+		logs.Log(logs.ERROR, "Unable to load environment variables: "+err.Error())
+	}
+	var message = os.Getenv("MESSAGE")
+	logs.Log(logs.INFO, "Dear Matron app running...")
+	logs.Log(logs.INFO, message)
 
 	handlers.StartServer()
 
